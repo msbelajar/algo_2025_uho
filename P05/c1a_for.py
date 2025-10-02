79645460423cb35c9e852da8f721f2c65ce0dbe9
muh.sadno <noe.mushashi@gmail.com>
2025-10-02 10:04:05 +0800

    "update"

--- P05/c1a_for.py
+++ P05/c1a_for.py
@@ -1,5 +1,5 @@
 print("Contoh 1")
-for _ in range(4):
+for _ in range(4): #0,1,2,3
     print('Hello')
 
 print("\nContoh 2")
@@ -7,8 +7,8 @@ for i in range(1,5):   #i = 1,2,3,4
     print(i)
 
 print("\nContoh 2a")
-for i in range(1,5):
-    print(i, end=' ')
+for i in range(1,5): #1,2,3,4
+    print(i, end=" ")
 
 print("\n\nContoh 3")
 for i in range(1,10,2): # i = 1,3,5,7,9
@@ -17,3 +17,5 @@ for i in range(1,10,2): # i = 1,3,5,7,9
 print("\nContoh 4")
 for j in range(10,1,-2): # i = 10,8,6,4,2
     print(j)
+
+
---
+++ P05/l01_genap.py
@@ -0,0 +1,5 @@
+#barisan bilangan genap 
+#dari 2 hingga bilangan asli n
+n = int(input("Input n = "))
+for i in range(2, n+1, 2):
+   print(i, end=" ")
