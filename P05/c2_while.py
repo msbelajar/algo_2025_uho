f1d00ff2f238fbd3b2df5773fbb7a2e6e1a0937b
muh.sadno <noe.mushashi@gmail.com>
2025-10-19 17:07:27 +0800

    "update"

--- P05/c2_while.py
+++ P05/c2_while.py
@@ -1,6 +1,6 @@
 #tampilkan n buah bilangan ganjil dari 1
 n = int(input("Input n = "))
-i = 1
-while i<=n:
-    print(2*i-1, end=" ")
+i = 0
+while i<n:
+    print(2*i+1, end=" ")
     i = i+1
---
+++ P05/c6_break.py
@@ -0,0 +1,3 @@
+#cetak bilangan dari 1 sampai n 
+#jika n = 9 hentikan perulangan
+n = int(input("n = "))
